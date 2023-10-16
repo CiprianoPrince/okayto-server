e1de03e6cdcecb7a9bd8dfea2a3bdf2f35fa9682
Prince <princetonjudecipriano@gmail.com>
2023-10-16 09:56:23 +0800

    fixed verifyJWT name import

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,7 @@
 module.exports = {
     credentials: require('./credentials.middleware'),
     errorLogger: require('./errorLogger.middleware'),
-    verifyJwt: require('./verifyJwt.middleware'),
+    verifyJwt: require('./verifyJWT.middleware'),
     verifyRoles: require('./verifyRoles.middleware'),
     uploadProductImage: require('./productMulter.middleware'),
     uploadVariantImage: require('./variantMulter.middleware'),
